f44afd210b1bd80c5f3b99f42608357965b44891
DanielJacobs30 <71023471+DanielJacobs30@users.noreply.github.com>
2020-10-14 16:02:44 +0200

    New push

---
+++ Assets/02_Student Folders/DanielJacobs_Assets/Scripts/Elevator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    //public GameObject moveplatform;
+
+
+    //private void Ontriggerstay()
+    // {
+    //   
+
+    //}
+    public GameObject Player;
+    public GameObject Player2;
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("werkt");
+        if (other.gameObject == Player2)
+        {
+            Player2.transform.parent = transform;
+            Debug.Log("werkt3");
+            Player.transform.position += Player.transform.up * Time.deltaTime;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("werkt2");
+        if (other.gameObject == Player2)
+        {
+            Debug.Log("werkt4");
+            Player2.transform.parent = null;
+        }
+    }
+}
