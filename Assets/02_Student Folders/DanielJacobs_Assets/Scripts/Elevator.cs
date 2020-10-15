3fc294f19521cc33e1792549b67a2d64cf853ace
DanielJacobs30 <71023471+DanielJacobs30@users.noreply.github.com>
2020-10-15 17:10:47 +0200

    New push

--- Assets/02_Student Folders/DanielJacobs_Assets/Scripts/Elevator.cs
+++ Assets/02_Student Folders/DanielJacobs_Assets/Scripts/Elevator.cs
@@ -13,25 +13,52 @@ public class Elevator : MonoBehaviour
     //   
 
     //}
+    public GameObject Cube;
     public GameObject Player;
-    public GameObject Player2;
-    private void OnTriggerStay(Collider other)
+
+    bool elevatorup = false;
+    float speed = 1.0f;
+    private void Update()
     {
-        Debug.Log("werkt");
-        if (other.gameObject == Player2)
+        if (Cube.transform.localPosition.y <= 16)
         {
-            Player2.transform.parent = transform;
-            Debug.Log("werkt3");
-            Player.transform.position += Player.transform.up * Time.deltaTime;
+            if (elevatorup == true)
+            {
+                Cube.transform.position += Cube.transform.up * Time.deltaTime;
+                
+            }
+        }
+        if (Cube.transform.localPosition.y >= 3.76)
+        {
+            if (elevatorup == false)
+            {
+                Cube.transform.position -= Cube.transform.up * Time.deltaTime;
+                
+            }
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("werkt");
+        if (other.gameObject == Player)
+        {
+           elevatorup = true;
+           
+            Player.transform.parent = transform;
+            Debug.Log("werkt3");
+            
+        }
+    }
+    
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("werkt2");
-        if (other.gameObject == Player2)
+        if (other.gameObject == Player)
         {
+            elevatorup = false;
             Debug.Log("werkt4");
-            Player2.transform.parent = null;
+            Player.transform.parent = null;
         }
     }
 }
+
